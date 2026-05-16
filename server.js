ed21bc52087bb9a629a44f89ff51ce3b0c3dbf4c
woodrow-snow <woodrow2021@outlook.com>
2026-05-16 07:24:22 -0600

    added cors for requests

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const express = require('express');
 const mongo = require('./data/database');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDoc = require('./swagger-output.json');
+const cors = require('cors');
 
 // creating app
 const app = express();
@@ -14,6 +15,7 @@ const app = express();
 // =============================
 app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
+app.use(cors());
 
 // =============================
 // = Routes
