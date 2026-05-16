31c0fc2f8a7c5f66b6dc316687b25e62fd5fb0bb
woodrow-snow <woodrow2021@outlook.com>
2026-05-15 18:04:15 -0600

    updated swagger config

--- swagger.js
+++ swagger.js
@@ -5,7 +5,11 @@ const doc = {
     title: 'Woodrow Snow Contacts API',
     description: 'This is the contacts API built in week 1 and 2 of CSE341'
   },
-  host: 'https://cse341-contacts-api-n92e.onrender.com'
+    servers: [
+        {
+          url: 'https://cse341-contacts-api-n92e.onrender.com'
+      }
+  ]
 };
 
 const outputFile = './swagger-output.json';
